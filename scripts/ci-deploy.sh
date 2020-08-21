8c44450cb2c6ef19bfe324b1bf30379d1ed966db
Paul Sherer <wolfy@wolfymaster.com>
2020-08-21 06:26:54 -0400

    im a dumb dumb

--- scripts/ci-deploy.sh
+++ scripts/ci-deploy.sh
@@ -7,6 +7,19 @@ SECRET_NAME=ecr-login-secret
 # Fetch token (which will expire in 12 hours)
 TOKEN=`aws ecr --region=$AWS_REGION get-authorization-token --output text --query authorizationData[].authorizationToken | base64 -d | cut -d: -f2`
 
+COMMIT_SHA1=$CIRCLE_SHA1
+
+# We must export it so it's available for envsubst
+export COMMIT_SHA1=$COMMIT_SHA1
+
+# since the only way for envsubst to work on files is using input/output redirection,
+#  it's not possible to do in-place substitution, so we need to save the output to another file
+#  and overwrite the original with that one.
+envsubst <./k8s/deployment.yml >./k8s/deployment.yml.out
+mv ./k8s/deployment.yml.out ./k8s/deployment.yml
+
+echo "$KUBERNETES_CLUSTER_CERTIFICATE" | base64 --decode > cert.crt
+
 # Create or replace registry secret
 ./kubectl --kubeconfig=/dev/null \
   --server=$KUBERNETES_SERVER \
@@ -23,20 +36,7 @@ TOKEN=`aws ecr --region=$AWS_REGION get-authorization-token --output text --quer
  --docker-username=AWS \
  --docker-password="${TOKEN}"
 
-
-COMMIT_SHA1=$CIRCLE_SHA1
-
-# We must export it so it's available for envsubst
-export COMMIT_SHA1=$COMMIT_SHA1
-
-# since the only way for envsubst to work on files is using input/output redirection,
-#  it's not possible to do in-place substitution, so we need to save the output to another file
-#  and overwrite the original with that one.
-envsubst <./k8s/deployment.yml >./k8s/deployment.yml.out
-mv ./k8s/deployment.yml.out ./k8s/deployment.yml
-
-echo "$KUBERNETES_CLUSTER_CERTIFICATE" | base64 --decode > cert.crt
-
+# Apply deployment
 ./kubectl \
   --kubeconfig=/dev/null \
   --server=$KUBERNETES_SERVER \
